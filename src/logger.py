f0fa45a795695c95b15e0f3101233aea087cd342
kassem <iqasem4444@gmail.com>
2025-08-05 19:31:28 +0300

    add logger

--- src/logger.py
+++ src/logger.py
@@ -5,7 +5,7 @@ def get_logger(name=None):
     logger.setLevel(logging.INFO)
     if not logger.handlers:
         handler = logging.StreamHandler()
-        formatter = logging.Formatter('[%(asctime)s] %(levelname)s - %(message)s')
+        formatter = logging.Formatter('[%(asctime)s] %(levelname)s %(filename)s - %(message)s')
         handler.setFormatter(formatter)
         logger.addHandler(handler)
     return logger
